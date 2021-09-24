b58640efe0b3eb865b13d8d400b70e1731306568
marlondemas <demasmarlon@gmail.com>
2021-09-24 23:16:35 +0200

    updated mysql connections

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: 'localhost',
-    USER: "root",
-    PASSWORD: "",
-    DB: "testdb",
+    HOST: 'us-cdbr-east-04.cleardb.com',
+    USER: "b474edca8f5e69",
+    PASSWORD: "d498c9d1",
+    DB: "heroku_050e1ea318114b7",
     dialect: "mysql",
     pool: {
         max: 5,
