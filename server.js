aa6ddb1bf8ba0ae601a55a2803a3b10e509277d0
marlondemas <demasmarlon@gmail.com>
2021-09-25 13:10:52 +0200

    updated default message on page load

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ db.sequelize.sync({ force: true }).then(() => {
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to Makes' application." });
 });
 
 require("./app/routes/tutorial.routes")(app);
